99aa6b23dcde516d14873b77af387269ac128b78
mebeborgit config --global user.namen <mebeborn@gmail.com>
2019-03-14 19:41:29 +0200

    make it better

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json())
 app.use('/api', apiRouter);
 app.use('/api/v2', apiRouterV2);
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  const server = server.address().port;
+  console.log(`Server running at ${server}/`);
 });
